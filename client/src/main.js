e4b639cd5daae5c154a2901f423dbc172732a8e4
zhirschtirtt <zhirschtritt@gmail.com>
2018-08-22 09:07:31 -0400

    fix: formatting

--- client/src/main.js
+++ client/src/main.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import Vuetify from "vuetify";
-import colors from "vuetify/es5/util/colors";
-import App from "./App.vue";
-import store from "./store";
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import colors from 'vuetify/es5/util/colors';
+import App from './App.vue';
+import store from './store';
 
 Vue.use(Vuetify, {
   theme: {
     primary: colors.purple.base,
     secondary: colors.grey.darken1,
     accent: colors.shades.black,
-    error: colors.red.accent3
-  }
+    error: colors.red.accent3,
+  },
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
