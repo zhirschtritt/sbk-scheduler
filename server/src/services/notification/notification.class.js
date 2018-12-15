2c3dbebc261ec42a6ae3ce58c09128a2a628ed7a
zhirschtirtt <zhirschtritt@gmail.com>
2018-12-15 13:56:57 -0500

    fix: add notification handlers

--- server/src/services/notification/notification.class.js
+++ server/src/services/notification/notification.class.js
@@ -1,86 +1,22 @@
-const moment = require('moment');
-
-/* eslint-disable no-unused-vars */
 class Service {
-
-  constructor ({mailer, staffMembers, shifts}) {
-    this.mailer = mailer;
-    this.staffMembers = staffMembers;
-    this.shifts = shifts;
+  constructor ({mailer, staffMembers, shifts, NotificationHandlerFactory, logger}) {
+    this.NotificationHandlerFactory = new NotificationHandlerFactory({logger, shifts, staffMembers, mailer});
+    this.log = logger;
   }
 
-  formatDate(date) {
-    return moment(date).format('dddd, MMM D, YYYY');
-  }
+  async create ({notificationType, message, context}) { 
+    const notificationHandler = this.NotificationHandlerFactory.manufacture({
+      notificationType, 
+      message,
+      context
+    });
 
-  async create (data, params) { 
-    // notification = {
-    //   notificationType: String,
-    //   message: String,
-    //   context: {
-    //     shift: Shift,
-    //     staffMember: StaffMember,
-    //   }
-    // }
-
-    // if a shift has been cleared by the client 
-    if (data && data.notificationType === 'cancelledShift') {
-      const { shift, staffMember } = data.context;
-      const { message } = data;
-
-      return await this.mailer.sendEmail({
-        template: {
-          name: 'cancelledShift',
-          context: {shift, staffMember, message}
-        },
-        recipients: this.mailer.staffEmail,
-        subject: `⚠️ Cancelled Upcoming Shift: ${this.formatDate(shift.date)}`
-      });
+    try {
+      return await notificationHandler.handle();
+    } catch (error) {
+      this.log.error('Error processing notification', error);
+      return new Error(error);
     }
-
-    // get next upcoming shift`
-    const today = moment().format('YYYY-MM-DD');
-    const endOfWeek = moment().add(7, 'days').format('YYYY-MM-DD');
-    const nextShifts = await this.shifts.find({query: {start: today, end: endOfWeek }});
-
-    return Promise.all(
-      nextShifts.map(async (shift) => {
-        const isStaffAssigned = shift.primary_staff || shift.secondary_staff;
-
-        // if no staffMembers assigned to upcoming shifts, draft email to staff
-        if (!isStaffAssigned) { 
-          return await this.mailer.sendEmail({
-            template: {
-              name: 'emptyShift',
-              context: shift
-            },
-            recipients: this.mailer.staffEmail,
-            subject: `⚠️ SBK Reminder: Unassigned Upcoming Shift ${this.formatDate(shift.date)}`
-          });
-        }
-
-        // else draft emails to all assigned staffMembers per shift
-        const allStaffMembers = await this.staffMembers.find();
-
-        const assignedStaffMemberEmails = allStaffMembers
-          .filter((staffMember) => {
-            return [shift.primary_staff, shift.secondary_staff]
-              .map((name) => name.toLowerCase())
-              .includes(staffMember.name.toLowerCase());
-          })
-          .filter((staffMember) => !!staffMember.notifications)
-          .map((staffMember) => staffMember.email);
-        
-        return await this.mailer.sendEmail({
-          template: {
-            name: 'upcomingShift',
-            context: shift,
-          },
-          recipients: assignedStaffMemberEmails,
-          subject: `👋 SBK Reminder: Upcoming Shift ${this.formatDate(shift.date)}`
-        });
-      })
-    );
   }
 }
 
@@ -89,3 +25,12 @@ module.exports = function (options) {
 };
 
 module.exports.Service = Service;
+
+// notification = {
+//   notificationType: String,
+//   message: String,
+//   context: {
+//     shift: Shift,
+//     staffMember: StaffMember,
+//   }
+// }   
