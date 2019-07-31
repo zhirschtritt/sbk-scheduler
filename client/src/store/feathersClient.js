54b7a63b766126da81902e98330546175731d4c2
zhirschtirtt <zhirschtritt@gmail.com>
2019-07-30 23:06:45 -0400

    fix: new path

--- client/src/store/feathersClient.js
+++ client/src/store/feathersClient.js
@@ -5,8 +5,11 @@ import io from 'socket.io-client';
 const feathersClient = feathers();
 
 const baseUrl =
-  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3030';
-const socket = io(baseUrl, { transports: ['websocket'] });
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3030';
+const socket = io(window.location.origin, {
+  transports: ['websocket'],
+  path: '/api/ws'
+});
 
 feathersClient.configure(socketio(socket));
 
