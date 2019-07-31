9665f380bff64077c1796a5e090f2484c4653e52
zhirschtirtt <zhirschtritt@gmail.com>
2019-07-30 22:43:13 -0400

    fix: correct base url

--- client/src/store/feathersClient.js
+++ client/src/store/feathersClient.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 const feathersClient = feathers();
 
 const baseUrl =
-  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3030';
+  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3030';
 const socket = io(baseUrl, { transports: ['websocket'] });
 
 feathersClient.configure(socketio(socket));
